f2c5d89c850ac15f8eff9902f985f44e1096db1d
Dima Birenbaum <dvlasenko86@gmail.com>
2020-05-02 17:37:31 +0300

    added dummy API end point

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 app = Flask(__name__, template_folder='templates')
 
 
@@ -17,6 +17,14 @@ def index():
         }
     ]
     return render_template('index.html', title='EnglishTips', user=user, posts=posts)
+
+
+@app.route('/api/test', methods=['POST'])
+def test():
+    content = request.get_json()
+    print(content)
+    response = {"Text": "Hello " + content['name'] + ", I know that you are " + str(content['age']) + " years old."}
+    return jsonify(response)
 
 
 if __name__ == '__main__':

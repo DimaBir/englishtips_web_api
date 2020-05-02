047ca20c68071a8fffdbfc8917163df1cd829bfe
Dima Birenbaum <dvlasenko86@gmail.com>
2020-05-02 14:07:44 +0300

    Hello world example

--- application.py
+++ application.py
@@ -1,7 +1,19 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import render_template
+from app import app
 
 
-@app.route("/")
-def hello():
-    return "Hello World!!!"
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)

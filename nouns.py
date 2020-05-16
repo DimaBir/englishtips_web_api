c16464fc301ce9666987968d4931e65b420b9c9a
DimaVlas <dvlasenko86@gmail.com>
2020-05-16 12:10:55 +0300

    Added indexer of words in sentence by chars

--- nouns.py
+++ nouns.py
@@ -1,17 +1,23 @@
+import re
 import nltk
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
 
-from utils import find_word_index
+from utils import find_word_index, find_first_char_index
 
 
 def find_nouns(text: None):
-    nltk.download('punkt')
-    nltk.download('averaged_perceptron_tagger')
-
     result = {}
     tokens = nltk.word_tokenize(text)
     nouns = [word for (word, pos) in nltk.pos_tag(tokens) if(pos[:2] == 'NN')]
 
     for noun in nouns:
-        result[noun] = find_word_index(text, noun, one_based=True)
+        splitted_nouns = re.split(r'\s+|[,;.-]\s*', noun)
+        for spl_noun in splitted_nouns:
+            if spl_noun in result.keys():
+                indexes = find_word_index(text, spl_noun, one_based=True)
+                result[spl_noun] = list(set(indexes) | set(result[spl_noun]))
+                continue
+            result[spl_noun] = find_word_index(text, spl_noun, one_based=True)
 
     return result

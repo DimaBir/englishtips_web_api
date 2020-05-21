78d70e217575624d983947b6d59c4bb4160170f7
DimaVlas <dvlasenko86@gmail.com>
2020-05-21 20:50:09 +0300

    Parallelization: re-added nltk download

--- application.py
+++ application.py
@@ -170,9 +170,4 @@ def translate():
 
 
 if __name__ == '__main__':
-    nltk.download('punkt')
-    nltk.download('wordnet')
-    nltk.download('stopwords')
-    nltk.download('averaged_perceptron_tagger')
-
     app.run(threaded=True)

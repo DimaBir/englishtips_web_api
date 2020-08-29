fd417c43638903189f924d000b753337f711cb66
DimaVlas <dvlasenko86@gmail.com>
2020-08-29 17:55:40 +0300

    Updated trailing slashes in routes

--- application.py
+++ application.py
@@ -165,7 +165,7 @@ def synonym():
         return str("Error: " + str(e))
 
 
-@app.route('/api/acr/', methods=['POST'])
+@app.route('/api/acr', methods=['POST'])
 def acronyms():
     try:
         start = timer()
@@ -192,7 +192,7 @@ def acronyms():
         return str("Error: " + str(e))
 
 
-@app.route('/api/hyper/', methods=['POST'])
+@app.route('/api/hyper', methods=['POST'])
 def hypernyms():
     try:
         start = timer()

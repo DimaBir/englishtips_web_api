e07f6b19f69ac32aab2869ab1e347649856a01e3
DimaBir <dvlasenko86@gmail.com>
2020-08-30 21:24:14 +0300

    Debug False

--- application.py
+++ application.py
@@ -66,8 +66,6 @@ def upload_file():
             flash('No file part')
             return redirect(request.url)
         file = request.files['file']
-        # if user does not select file, browser also
-        # submit an empty part without filename
         if file.filename == '':
             flash('No selected file', 'warning')
             return redirect(request.url)
@@ -335,5 +333,5 @@ if __name__ == '__main__':
 
     context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
     context.load_cert_chain('avrl_cs_technion_ac_il.crt', 'AVRL_cs_technion_ac_il.key')
-    app.run(host="0.0.0.0", port=80, ssl_context=context, threaded=True, debug=True)
+    app.run(host="0.0.0.0", port=80, ssl_context=context, threaded=True, debug=False)
     # app.run(debug=True)

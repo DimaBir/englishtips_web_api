b95d6d0a420c71f466cf5d4a534871c482d5c1dd
DimaBir <dvlasenko86@gmail.com>
2020-08-31 20:54:19 +0300

    Added ASL, Sentence Structure

--- asl.py
+++ asl.py
@@ -1,7 +1,10 @@
 import nltk
 import numpy as np
 
+from nltk.tokenize import RegexpTokenizer
+
 
 def avg_sentence_len(text: str) -> float:
-    sent_lens = [len(nltk.word_tokenize(sent)) for sent in nltk.sent_tokenize(text)]
+    tokenizer = RegexpTokenizer(r'\w+')
+    sent_lens = [len(tokenizer.tokenize(sent)) for sent in nltk.sent_tokenize(text)]
     return np.mean(sent_lens)

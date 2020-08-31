0a78c1dd8ed0676a28ad4dc9941e1bf04d5b56ad
DimaBir <dvlasenko86@gmail.com>
2020-08-31 14:33:18 +0300

    Added admin placeholder. add/list/delete to the database

--- application.py
+++ application.py
@@ -341,9 +341,9 @@ def translate():
 
 
 if __name__ == '__main__':
-    # import ssl
-    #
-    # context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-    # context.load_cert_chain('avrl_cs_technion_ac_il.crt', 'AVRL_cs_technion_ac_il.key')
-    # app.run(host="0.0.0.0", port=80, ssl_context=context, threaded=True, debug=False)
-    app.run(debug=True)
+    import ssl
+
+    context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+    context.load_cert_chain('avrl_cs_technion_ac_il.crt', 'AVRL_cs_technion_ac_il.key')
+    app.run(host="0.0.0.0", port=80, ssl_context=context, threaded=True, debug=False)
+    # app.run(debug=True)

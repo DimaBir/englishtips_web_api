cbf1e47ebe661f0bb3897edaac9c274c90960678
DimaVlas <dvlasenko86@gmail.com>
2020-09-19 20:01:26 +0300

    Added useful phrases

--- application.py
+++ application.py
@@ -394,11 +394,16 @@ def avg_sent_len():
         return str("Error: " + str(e))
 
 
-@app.route('/api/useful', methods=['POST'])
+@app.route('/api/useful', methods=['GET'])
 def useful_phrases():
     try:
         useful_phrases_dictionary = get_useful_phrase()
-        return json.dumps(useful_phrases_dictionary)
+        result = {
+            "keys": list(useful_phrases_dictionary.keys()),
+            "examples": list(useful_phrases_dictionary.values())
+        }
+
+        return json.dumps(result)
     except Exception as e:
         return str("Error: " + str(e))
 
@@ -416,9 +421,9 @@ def translate():
 
 
 if __name__ == '__main__':
-    # import ssl
-    #
-    # context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-    # context.load_cert_chain('avrl_cs_technion_ac_il.crt', 'AVRL_cs_technion_ac_il.key')
-    # app.run(host="0.0.0.0", port=80, ssl_context=context, threaded=True, debug=False)
-    app.run(debug=True)
+    import ssl
+
+    context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+    context.load_cert_chain('avrl_cs_technion_ac_il.crt', 'AVRL_cs_technion_ac_il.key')
+    app.run(host="0.0.0.0", port=80, ssl_context=context, threaded=True, debug=False)
+    # app.run(debug=True)

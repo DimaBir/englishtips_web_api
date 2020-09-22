8da660f89cd6dc8d481ad795fc1bf176bf83336c
DimaVlas <dvlasenko86@gmail.com>
2020-09-22 15:30:54 +0300

    rounded ASL

--- logic/analytics/asl.py
+++ logic/analytics/asl.py
@@ -7,4 +7,4 @@ from nltk.tokenize import RegexpTokenizer
 def avg_sentence_len(text: str) -> float:
     tokenizer = RegexpTokenizer(r'\w+')
     sent_lens = [len(tokenizer.tokenize(sent)) for sent in nltk.sent_tokenize(text)]
-    return np.mean(sent_lens)
+    return round(np.mean(sent_lens), 2)

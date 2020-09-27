f7ed0a2e7ba3457a7e031d0aebcb85907ef36b77
Dima Birenbaum <dvlasenko86@gmail.com>
2020-09-27 15:30:22 +0300

    Update application.py

--- application.py
+++ application.py
@@ -2,9 +2,9 @@ from app import app
 
 
 if __name__ == '__main__':
-    import ssl
+    # import ssl
 
-    context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-    context.load_cert_chain('avrl_cs_technion_ac_il.crt', 'AVRL_cs_technion_ac_il.key')
-    app.run(host="0.0.0.0", port=80, ssl_context=context, threaded=True, debug=False)
-    # app.run(debug=True)
+    # context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+    # context.load_cert_chain('avrl_cs_technion_ac_il.crt', 'AVRL_cs_technion_ac_il.key')
+    # app.run(host="0.0.0.0", port=80, ssl_context=context, threaded=True, debug=False)
+    app.run(threaded=True, debug=False)

be13c9b6f129f9db206242f871e5941653527f5c
DimaVlas <dvlasenko86@gmail.com>
2020-12-12 23:18:22 +0200

    Changed the label

--- app/forms.py
+++ app/forms.py
@@ -4,5 +4,5 @@ from wtforms import StringField, SubmitField
 
 class PredictForm(FlaskForm):
 
-    sentence = StringField("Please, type sentence to check.")
+    sentence = StringField("Please, type sentence to check for wordiness.")
     submit = SubmitField("Check")

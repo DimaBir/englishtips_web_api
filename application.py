7d439458159d1656ed11dbb95d961fe05375b386
Dima Birenbaum <dvlasenko86@gmail.com>
2020-12-12 17:52:44 +0200

    Latest

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ from app import app
 from gevent.pywsgi import WSGIServer
 
 if __name__ == "__main__":
-    # http_server = WSGIServer(('0.0.0.0', 5000), app, keyfile='AVRL_cs_technion_ac_il.key',
-    #                          certfile='avrl_cs_technion_ac_il.crt')
-    # http_server.serve_forever()
-    app.run(debug=True)
+    http_server = WSGIServer(('0.0.0.0', 80), app, keyfile='AVRL_cs_technion_ac_il.key',
+                             certfile='avrl_cs_technion_ac_il.crt')
+    http_server.serve_forever()
+    # app.run(debug=True)

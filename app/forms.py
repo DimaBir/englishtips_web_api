f489502afae60de8dc1671e258b0b79183445f6a
DimaVlas <dvlasenko86@gmail.com>
2020-12-13 15:11:15 +0200

    Removed captcha

--- app/forms.py
+++ app/forms.py
@@ -6,20 +6,20 @@ from wtforms.validators import DataRequired, Length
 class PredictForm(FlaskForm):
 
     sentence = StringField("Please, type sentence to check for wordiness.")
-    recaptcha = RecaptchaField()
+    # recaptcha = RecaptchaField()
     submit = SubmitField("Check")
 
 
 class DatasetForm(FlaskForm):
 
-    sentence = StringField("Please, enter the sentence, with dot at the end.", DataRequired())
+    sentence = StringField("Please, enter the sentence, with dot at the end.",)
     label = SelectField(
-        'Class',
+        'Class:',
         [DataRequired()],
         choices=[
-            ('Wordy', '0'),
-            ('Clear', '1')
+            ('1', 'Wordy'),
+            ('0', 'Clear')
         ]
     )
-    recaptcha = RecaptchaField()
+    # recaptcha = RecaptchaField()
     submit = SubmitField("Add")
